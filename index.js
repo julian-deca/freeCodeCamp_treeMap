d0c11ef8a63f5eb5d97aa9a5dfa7de2f8b151f0b
julian <juliandecastelli@gmail.com>
2022-08-08 12:17:45 -0300

    add genres and main structure

--- index.js
+++ index.js
@@ -25,4 +25,38 @@ async function getData() {
 }
 async function proceede() {
   const fullData = await getData();
+  const action = fullData.children[0];
+  const drama = fullData.children[1];
+  const adventure = fullData.children[2];
+  const family = fullData.children[3];
+  const animation = fullData.children[4];
+  const comedy = fullData.children[5];
+  const biography = fullData.children[6];
+
+  const block = svg.append("g");
+  console.log(action);
+  block
+    .selectAll("g")
+    .data(fullData.children)
+    .enter()
+    .append("g")
+    .attr("genre", (d) => d.name)
+    .selectAll("rect")
+    .data((d) => d.children)
+    .enter()
+    .append("rect")
+    .attr("width", 250)
+    .attr("height", 250)
+    .attr("x", 250)
+    .attr("y", 250);
+
+  /*.selectAll("rect")
+    .data(fullData.children)
+    .enter()
+    .append("rect")
+    .attr("width", 250)
+    .attr("height", 250)
+    .attr("x", 250)
+    .attr("y", 250);*/
+  console.log(fullData);
 }
